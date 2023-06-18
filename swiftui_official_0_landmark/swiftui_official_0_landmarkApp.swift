57a8afa8dfea7280864b9540fff2838847d87b58
Jun Young Bang <bhang96@gmail.com>
2023-06-18 15:40:54 +0800

    Initial Commit

---
+++ swiftui_official_0_landmark/swiftui_official_0_landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_official_0_landmarkApp.swift
+//  swiftui_official_0_landmark
+//
+//  Created by June on 18/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_official_0_landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
